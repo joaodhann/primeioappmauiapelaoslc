a6aaae3b4f25168fc4cd9ccef4f459e9ec065124
Usuario <Usuario@CPD03-M15>
2024-02-19 15:53:05 -0300

    Adicionar arquivos de projeto.

---
+++ primeioappmauiapelaoslc/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace primeioappmauiapelaoslc
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ primeioappmauiapelaoslc/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace primeioappmauiapelaoslc
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
